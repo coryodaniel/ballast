39913df52e2eff9a06fac0d2b6fd0b3fe0557cfe
Cory O'Daniel <cory@coryodaniel.com>
2019-06-20 15:03:06 -0700

    removing cross clusters support

--- lib/ballast/pool_policy/managed_pool.ex
+++ lib/ballast/pool_policy/managed_pool.ex
@@ -16,17 +16,13 @@ defmodule Ballast.PoolPolicy.ManagedPool do
   Parse resource `managed_pool` spec and annotate with `NodePool` data from API.
   """
   @spec new(map(), binary(), binary()) :: {:ok, t()} | {:error, atom}
-  def new(managed_pool_spec, project, source_cluster) do
+  def new(managed_pool_spec, project, cluster) do
     %{
       "minimumPercent" => mp,
       "minimumInstances" => mi,
       "poolName" => name,
       "location" => location
     } = managed_pool_spec
-
-    # Support managed pools in different clusters than the source pool's cluster.
-    # If not set then the pool is expected to be the in the source pool's cluster
-    cluster = Map.get(managed_pool_spec, "clusterName", source_cluster)
 
     pool = NodePool.new(project, location, cluster, name)
 
