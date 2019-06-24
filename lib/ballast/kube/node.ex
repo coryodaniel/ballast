e8f8e68f59c18b31bdda5122167e2519f22d57ed
Cory O'Daniel <cory@coryodaniel.com>
2019-06-24 14:27:53 -0700

    Node memory/cpu detection

--- lib/ballast/kube/node.ex
+++ lib/ballast/kube/node.ex
@@ -13,7 +13,8 @@ defmodule Ballast.Kube.Node do
     "DiskPressure"
   ]
 
-  @spec ready?(map()) :: boolean()
+  alias K8s.Resource
+
   @doc """
   Checks if `status.conditions` are present and node is `Ready`
 
@@ -51,6 +52,7 @@ defmodule Ballast.Kube.Node do
       ...> Ballast.Kube.Node.ready?(node)
       false
   """
+  @spec ready?(map()) :: boolean()
   def ready?(%{"status" => %{"conditions" => conditions}} = _node) do
     conditions
     |> find_condition_by_type("Ready")
@@ -59,7 +61,70 @@ defmodule Ballast.Kube.Node do
 
   def ready?(_), do: false
 
-  @spec resources_constrained?(map) :: boolean
+  @doc """
+  Finds the node with the most CPU
+
+  ## Examples
+      iex> node1 = %{"metadata" => %{"name" => "foo"}, "status" => %{"allocatable" => %{"cpu" => "940m"}}}
+      ...> node2 = %{"metadata" => %{"name" => "bar"}, "status" => %{"allocatable" => %{"cpu" => "1"}}}
+      ...> Ballast.Kube.Node.with_most_cpu([node1, node2])
+      %{"metadata" => %{"name" => "bar"},"status" => %{"allocatable" => %{"cpu" => "1"}}}
+  """
+  @spec with_most_cpu(list(map)) :: map
+  def with_most_cpu(nodes) do
+    initial = {0, nil}
+
+    {_highest, node} =
+      Enum.reduce(nodes, initial, fn node, {highest, _} = acc ->
+        cpu =
+          node
+          |> get_in(["status", "allocatable", "cpu"])
+          |> Resource.cpu()
+
+        case cpu > highest do
+          true ->
+            {cpu, node}
+
+          false ->
+            acc
+        end
+      end)
+
+    node
+  end
+
+  @doc """
+  Finds the node with the most memory
+
+  ## Examples
+      iex> node1 = %{"metadata" => %{"name" => "foo"}, "status" => %{"allocatable" => %{"memory" => "10Gi"}}}
+      ...> node2 = %{"metadata" => %{"name" => "bar"}, "status" => %{"allocatable" => %{"memory" => "3Gi"}}}
+      ...> Ballast.Kube.Node.with_most_memory([node1, node2])
+      %{"metadata" => %{"name" => "foo"}, "status" => %{"allocatable" => %{"memory" => "10Gi"}}}
+  """
+  @spec with_most_memory(list(map)) :: map
+  def with_most_memory(nodes) do
+    initial = {0, nil}
+
+    {_highest, node} =
+      Enum.reduce(nodes, initial, fn node, {highest, _} = acc ->
+        memory =
+          node
+          |> get_in(["status", "allocatable", "memory"])
+          |> Resource.memory()
+
+        case memory > highest do
+          true ->
+            {memory, node}
+
+          false ->
+            acc
+        end
+      end)
+
+    node
+  end
+
   @doc """
   Check the node's conditions to see if they are contrained, under pressure, or insufficient.
 
@@ -86,9 +151,9 @@ defmodule Ballast.Kube.Node do
 
     Is not constrained when an unknown condition is constrained
   """
+  @spec resources_constrained?(map) :: boolean
   def resources_constrained?(node), do: !!first_constrained_condition(node)
 
-  @spec first_constrained_condition(map) :: map | nil
   @doc """
   Returns the first constrained condition
 
@@ -107,6 +172,7 @@ defmodule Ballast.Kube.Node do
       ...> Ballast.Kube.Node.first_constrained_condition(node)
       %{"type" => "MemoryPressure", "status" => "True"}
   """
+  @spec first_constrained_condition(map) :: map | nil
   def first_constrained_condition(%{"status" => %{"conditions" => conditions}} = _node) do
     Enum.find(conditions, fn %{"type" => type} = condition ->
       case condition_has_status?(condition, "True") do
